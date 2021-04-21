43bebf9e7a96d51102eb39f935c51922720870ed
sepeca15 <sepeca15@gmail.com>
2021-04-21 12:46:39 +0000

    subio ej ya echo

--- src/app.js
+++ src/app.js
@@ -9,3 +9,21 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+var pronoun = ["the", "our"];
+var adj = ["great", "big"];
+var noun = ["jogger", "racoon"];
+var ext = [".io", ".org", ".net", ".com"];
+
+for (let index = 0; index < pronoun.length; index++) {
+  const priPart = pronoun[index];
+  for (let index = 0; index < adj.length; index++) {
+    const segPart = adj[index];
+    for (let index = 0; index < noun.length; index++) {
+      const terPart = noun[index];
+      for (let index = 0; index < ext.length; index++) {
+        const cuaPart = ext[index];
+        console.log(priPart + segPart + terPart + cuaPart);
+      }
+    }
+  }
+}
