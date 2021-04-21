2f705e0fb142d713a5472536253021393caf6aa7
sepeca15 <sepeca15@gmail.com>
2021-04-21 13:39:23 +0000

    Ejercicio ok

--- src/app.js
+++ src/app.js
@@ -13,7 +13,8 @@ var pronoun = ["the", "our"];
 var adj = ["great", "big"];
 var noun = ["jogger", "racoon"];
 var ext = [".io", ".org", ".net", ".com"];
-
+var dominiosP = document.getElementById("dominiosP");
+var dominio = [];
 for (let index = 0; index < pronoun.length; index++) {
   const priPart = pronoun[index];
   for (let index = 0; index < adj.length; index++) {
@@ -22,8 +23,11 @@ for (let index = 0; index < pronoun.length; index++) {
       const terPart = noun[index];
       for (let index = 0; index < ext.length; index++) {
         const cuaPart = ext[index];
-        console.log(priPart + segPart + terPart + cuaPart);
+        // console.log(priPart + segPart + terPart + cuaPart);
+        dominio.push(priPart + segPart + terPart + cuaPart);
+        dominiosP.innerHTML += priPart + segPart + terPart + cuaPart + "<br>";
       }
     }
   }
 }
+console.log(dominio);
